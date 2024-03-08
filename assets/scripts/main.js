149db0f17ddc6bf5c38cedda893466258729748a
Khawla <96916652+khawla-14@users.noreply.github.com>
2024-03-08 20:41:00 +0100

    Added bg

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,8 +6,10 @@ class Game {
         this.height = this.canvas.height;
         this.baseHeight = 720;
         this.ratio = this.height / this.baseHeight;
+        this.background = new Background(this); 
         this.player = new Player(this);
         this.gravity;
+        this.speed;
 
         this.resize(window.innerWidth, window.innerHeight);
 
@@ -40,9 +42,13 @@ class Game {
         this.ratio = this.height / this.baseHeight;
 
         this.gravity = 0.15 * this.ratio;
+        this.speed = 3;
+        this.background.resize();
         this.player.resize();
     }
     render() {
+        this.background.update();
+        this.background.draw();
         this.player.update();
         this.player.draw();
     }
